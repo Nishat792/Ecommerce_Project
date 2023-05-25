545db1951a38fe725cf57e37c5a6ba0012a93d96
Sadia Zaman Nishat <131149262+Nishat792@users.noreply.github.com>
2023-05-25 16:21:02 +0600

    Update index.php

--- index.php
+++ index.php
@@ -89,9 +89,9 @@
         <section class="slider_area row m0">
             <div class="slider_inner">
                 <div class="camera_caption">
-                    <h4 class="wow fadeInUp animated" data-wow-delay="0.3s">Make Your Shopping Easy</h4>
                     <h5 class="wow fadeIn animated" data-wow-delay="0.3s">Find everything accordingly</h5>
                     <a class="learn_mor wow fadeInU" data-wow-delay="0.6s" href="#product-list">Buy Now!</a>
+                    <h4 class="wow fadeInUp animated" data-wow-delay="0.3s">Make Your Shopping Easy</h4>
                 </div>
             </div>
         </section>
